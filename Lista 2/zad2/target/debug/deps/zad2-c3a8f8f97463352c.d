506581290a701ea8f39a48f147aa24b90175c300
ovezthaking <oliwerx12@gmail.com>
2025-03-11 12:55:29 +0100

    zad2 l2 has been began

---
+++ Lista 2/zad2/target/debug/deps/zad2-c3a8f8f97463352c.d
@@ -0,0 +1,5 @@
+/home/oliwer/.ssh/REPOS/Jezyki-programowania-i-GIU/Lista 2/zad2/target/debug/deps/libzad2-c3a8f8f97463352c.rmeta: src/main.rs
+
+/home/oliwer/.ssh/REPOS/Jezyki-programowania-i-GIU/Lista 2/zad2/target/debug/deps/zad2-c3a8f8f97463352c.d: src/main.rs
+
+src/main.rs:
---
+++ Lista 2/zad2/target/debug/zad2.d
@@ -0,0 +1,1 @@
+/home/oliwer/.ssh/REPOS/Jezyki-programowania-i-GIU/Lista\ 2/zad2/target/debug/zad2: /home/oliwer/.ssh/REPOS/Jezyki-programowania-i-GIU/Lista\ 2/zad2/src/main.rs
