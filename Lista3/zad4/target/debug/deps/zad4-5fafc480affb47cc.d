c7c67e127b7e0c1c5416fcca6a65d275c9579b3e
ovezthaking <oliwerx12@gmail.com>
2025-03-29 23:23:48 +0100

    zad4 cleaned

--- Lista3/zad4/target/debug/deps/zad4-5fafc480affb47cc.d
+++
@@ -1,5 +0,0 @@
-/home/oliwer/.ssh/REPOS/Jezyki-programowania-i-GIU/Lista3/zad4/target/debug/deps/zad4-5fafc480affb47cc: src/main.rs
-
-/home/oliwer/.ssh/REPOS/Jezyki-programowania-i-GIU/Lista3/zad4/target/debug/deps/zad4-5fafc480affb47cc.d: src/main.rs
-
-src/main.rs:
