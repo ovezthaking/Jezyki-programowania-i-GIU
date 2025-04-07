8750fa918405f3ae1b1a9ee13b1c948ea8187270
ovezthaking <oliwerx12@gmail.com>
2025-04-07 21:21:35 +0200

    Test 1?

--- testing/target/debug/deps/testing-0a7d8af0530b11ab.d
+++
@@ -1,5 +0,0 @@
-/home/oliwer/.ssh/REPOS/Jezyki-programowania-i-GIU/testing/target/debug/deps/libtesting-0a7d8af0530b11ab.rmeta: src/main.rs
-
-/home/oliwer/.ssh/REPOS/Jezyki-programowania-i-GIU/testing/target/debug/deps/testing-0a7d8af0530b11ab.d: src/main.rs
-
-src/main.rs:
