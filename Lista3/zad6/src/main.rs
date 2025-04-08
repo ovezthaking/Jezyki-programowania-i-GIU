16b9bea20c5d3b9dac2bc91af2064caf29367a66
ovezthaking <oliwerx12@gmail.com>
2025-04-08 02:16:01 +0200

    zad6 in progress

--- Lista3/zad6/src/main.rs
+++ Lista3/zad6/src/main.rs
@@ -1,17 +1,25 @@
 use std::fs::File;
 use std::io::{self, BufRead, BufReader, Write};
+use flo_draw::*;
+use flo_canvas::*;
+use std::time::Duration;
 
 trait Fig {
     fn save(&self, w: &mut dyn Write) -> io::Result<()>;
     fn as_string(&self) -> String;
+    fn paint(&self, canvas: &mut dyn DrawingTarget);
 }
 
 struct Circle {
     radius: f64,
+    x: f64,
+    y: f64,
 }
 
 struct Square {
     side: f64,
+    x: f64,
+    y: f64,
 }
 
 
@@ -50,8 +58,7 @@ impl Fig for Rectangle {
     }
 }
 
-// -----------------------------
-// SAVE
+
 fn save_figures(figures: &[Box<dyn Fig>], path: &str) -> io::Result<()> {
     let mut file = File::create(path)?;
     for fig in figures {
@@ -60,8 +67,7 @@ fn save_figures(figures: &[Box<dyn Fig>], path: &str) -> io::Result<()> {
     Ok(())
 }
 
-// -----------------------------
-// LOAD
+
 fn load_figures(path: &str) -> io::Result<Vec<Box<dyn Fig>>> {
     let file = File::open(path)?;
     let reader = BufReader::new(file);
@@ -95,8 +101,7 @@ fn load_figures(path: &str) -> io::Result<Vec<Box<dyn Fig>>> {
     Ok(figures)
 }
 
-// -----------------------------
-// MAIN TEST
+
 fn main() -> io::Result<()> {
     let figures: Vec<Box<dyn Fig>> = vec![
         Box::new(Circle { radius: 5.0 }),
