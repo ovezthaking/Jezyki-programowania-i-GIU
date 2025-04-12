11d2902be2cfc8d38843cddd5b5f8c2e0cbf8ec7
ovezthaking <oliwerx12@gmail.com>
2025-04-12 21:50:59 +0200

    L3 zad6 completed

--- Lista3/zad6/src/main.rs
+++ Lista3/zad6/src/main.rs
@@ -2,12 +2,12 @@ use std::fs::File;
 use std::io::{self, BufRead, BufReader, Write};
 use::flo_canvas::*;
 use flo_draw::*;
-use std::thread;
 
-trait Fig {
+
+trait Fig: Send + Sync {
     fn save(&self, w: &mut dyn Write) -> io::Result<()>;
     fn as_string(&self) -> String;
-    fn paint(&self);
+    fn paint(&self, canvas: &DrawingTarget);
 }
 
 struct Circle {
@@ -32,31 +32,26 @@ impl Fig for Circle {
         format!("Circle({})", self.radius)
     }
 
-    fn paint(&self) {
-        let radius = self.radius; 
-        with_2d_graphics(move || {
-            
-            let canvas = create_drawing_window("Circle");
+    fn paint(&self, canvas: &DrawingTarget) {
+        let radius = self.radius;
+        canvas.draw(|gc| {
+            gc.clear_canvas(Color::Rgba(1.0, 1.0, 1.0, 1.0)); // czyścimy płótno
 
-            
-            canvas.draw(|gc| {
-                
-                gc.canvas_height(1000.0);
-                gc.center_region(0.0, 0.0, 1000.0, 1000.0);
+            gc.canvas_height(1000.0);
+            gc.center_region(0.0, 0.0, 1000.0, 1000.0);
 
-                
-                gc.new_path();
-                gc.circle(500.0, 500.0, radius as f32); 
+            gc.new_path();
+            gc.circle(500.0, 500.0, radius as f32);
 
-                gc.fill_color(Color::Rgba(0.3, 0.6, 0.8, 1.0));
-                gc.fill();
+            gc.fill_color(Color::Rgba(0.3, 0.6, 0.8, 1.0));
+            gc.fill();
 
-                gc.line_width(6.0);
-                gc.stroke_color(Color::Rgba(0.0, 0.0, 0.0, 1.0));
-                gc.stroke();
-            });
+            gc.line_width(6.0);
+            gc.stroke_color(Color::Rgba(0.0, 0.0, 0.0, 1.0));
+            gc.stroke();
         });
     }
+
 }
 
 impl Fig for Square {
@@ -68,29 +63,23 @@ impl Fig for Square {
         format!("Square({})", self.side)
     }
 
-    fn paint(&self) {
-        let side = self.side; 
-        with_2d_graphics(move || {
-            
-            let canvas = create_drawing_window("Square");
+    fn paint(&self, canvas: &DrawingTarget) {
+        let side = self.side;
+        canvas.draw(|gc| {
+            gc.clear_canvas(Color::Rgba(1.0, 1.0, 1.0, 1.0)); // czyszczenie
 
-            
-            canvas.draw(|gc| {
-                
-                gc.canvas_height(1000.0);
-                gc.center_region(0.0, 0.0, 1000.0, 1000.0);
+            gc.canvas_height(1000.0);
+            gc.center_region(0.0, 0.0, 1000.0, 1000.0);
 
-                
-                gc.new_path();
-                gc.rect(500.0, 500.0, side as f32, side as f32); 
+            gc.new_path();
+            gc.rect(500.0, 500.0, side as f32, side as f32);
 
-                gc.fill_color(Color::Rgba(0.3, 0.6, 0.8, 1.0));
-                gc.fill();
+            gc.fill_color(Color::Rgba(0.3, 0.6, 0.8, 1.0));
+            gc.fill();
 
-                gc.line_width(6.0);
-                gc.stroke_color(Color::Rgba(0.0, 0.0, 0.0, 1.0));
-                gc.stroke();
-            });
+            gc.line_width(6.0);
+            gc.stroke_color(Color::Rgba(0.0, 0.0, 0.0, 1.0));
+            gc.stroke();
         });
     }
 }
@@ -104,31 +93,24 @@ impl Fig for Rectangle {
         format!("Rectangle({}, {})", self.a, self.b)
     }
 
-    fn paint(&self) {
-        let a = self.a; 
-        let b = self.b; 
-    
-        with_2d_graphics(move || {
-            // Create a window
-            let canvas = create_drawing_window("Rectangle");
-    
-        
-            canvas.draw(|gc| {
-                
-                gc.canvas_height(1000.0);
-                gc.center_region(0.0, 0.0, 1000.0, 1000.0);
-    
-                
-                gc.new_path();
-                gc.rect(500.0, 500.0, a as f32, b as f32); 
-    
-                gc.fill_color(Color::Rgba(0.3, 0.6, 0.8, 1.0));
-                gc.fill();
-    
-                gc.line_width(6.0);
-                gc.stroke_color(Color::Rgba(0.0, 0.0, 0.0, 1.0));
-                gc.stroke();
-            });
+    fn paint(&self, canvas: &DrawingTarget) {
+        let a = self.a;
+        let b = self.b;
+        canvas.draw(|gc| {
+            gc.clear_canvas(Color::Rgba(1.0, 1.0, 1.0, 1.0)); // czyszczenie
+
+            gc.canvas_height(1000.0);
+            gc.center_region(0.0, 0.0, 1000.0, 1000.0);
+
+            gc.new_path();
+            gc.rect(500.0, 500.0, a as f32, b as f32);
+
+            gc.fill_color(Color::Rgba(0.3, 0.6, 0.8, 1.0));
+            gc.fill();
+
+            gc.line_width(6.0);
+            gc.stroke_color(Color::Rgba(0.0, 0.0, 0.0, 1.0));
+            gc.stroke();
         });
     }
 }
@@ -177,21 +159,36 @@ fn load_figures(path: &str) -> io::Result<Vec<Box<dyn Fig>>> {
 
 
 
+
+
 pub fn main() {
     let circle = Circle { radius: 50.0 };
     let square = Square { side: 100.0 };
     let rectangle = Rectangle { a: 150.0, b: 300.0 };
+    let rectangle2 = Rectangle { a: 300.0, b: 600.0 };
 
-    let figures: Vec<Box<dyn Fig>> = vec![Box::new(circle), Box::new(square), Box::new(rectangle)];
+    let figures: Vec<Box<dyn Fig>> = vec![Box::new(circle), Box::new(square), Box::new(rectangle), Box::new(rectangle2)];
     save_figures(&figures, "figures.txt").unwrap();
 
     let loaded_figures = load_figures("figures.txt").unwrap();
 
-    for fig in &loaded_figures {
-        println!("{}", fig.as_string());
-    }
+    with_2d_graphics(move || {
+        let canvas = create_drawing_window("Figury");
 
-    for fig in loaded_figures {
-        fig.paint();
-    }
+        let mut counter = 0;
+        for fig in &loaded_figures {
+            counter += 1;
+            if counter > loaded_figures.len() {
+                break;
+            }
+            else{
+                println!("{}", fig.as_string());
+                fig.paint(&canvas);
+                println!("Naciśnij Enter, żeby wyświetlić następną figurę...");
+                let _ = io::stdin().read_line(&mut String::new());
+            }
+            
+
+        }
+    });
 }
